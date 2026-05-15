3fde70340540e546218cdbc9726109122e33a7f9
Springtrap <springtrap@gmail.com>
2026-05-15 23:00:10 +0500

    Обновлен models.py

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import ForeignKey
 from sqlalchemy import String
 from datetime import datetime
 
@@ -18,6 +19,37 @@ class ChatRequest(Base):
     prompt: Mapped[str]
 
     response: Mapped[str]
+
+    created_at: Mapped[datetime] = mapped_column(
+        default=datetime.utcnow
+    )
+
+class Chat(Base):
+    __tablename__ = "chats"
+
+    id: Mapped[int] = mapped_column(
+        primary_key=True
+    )
+
+    ip_address: Mapped[str]
+
+    created_at: Mapped[datetime] = mapped_column(
+        default=datetime.utcnow
+    )
+
+class Message(Base):
+    __tablename__ = "messages"
+
+    id: Mapped[int] = mapped_column(
+        primary_key=True
+    )
+
+    chat_id: Mapped[int] = mapped_column(
+        ForeignKey("chats.id")
+    )
+
+    role: Mapped[str]
+    content: Mapped[str]
 
     created_at: Mapped[datetime] = mapped_column(
         default=datetime.utcnow
